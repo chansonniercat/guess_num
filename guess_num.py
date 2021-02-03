a99e51c6ffeff29020412b0b252885b95cf0700d
Cathy M <chansonniercat@gmail.com>
2021-02-03 21:57:21 +0800

    added count

--- guess_num.py
+++ guess_num.py
@@ -1,9 +1,12 @@
 import random
 
 r = random.randint(1,5)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('Please guess a number: ')
 	num = int(num)
+	print ('This is your', count,'guess.')
 	if num == r:
 		print('Bingo')
 		break
@@ -11,3 +14,4 @@ while True:
 		print('Answer is smaller')
 	elif num < r:
 		print('Answer is larger')
+	
